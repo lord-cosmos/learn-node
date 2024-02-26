0396621e93f322487aac03cfd68a7c9c63df6012
lord-cosmos <teslaatozinspires7@gmail.com>
2024-02-26 15:33:24 -0500

    add answer to fytd-poll

--- eventloop/poll.js
+++ eventloop/poll.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function someAsyncOperation() {
-  fs.readFile('/path/to/file', function(err, data) {
-    if (err) console.log('Read Error');
-    else console.log('Data: ' + data);
+  fs.readFile("/path/to/file", function (err, data) {
+    if (err) console.log("Read Error");
+    else console.log("Data: " + data);
   });
 }
 
 function foo() {
-  console.log('foo');
+  console.log("foo");
 }
 
 someAsyncOperation();
 foo();
-console.log('done');
+console.log("done");
